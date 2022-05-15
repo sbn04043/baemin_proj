65190a1376a59c795a283399a2e83e4fd1564331
sbn04043 <sbn04043@gmail.com>
2022-05-15 11:52:54 +0900

    초기 설정

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,31 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+import BootstrapVue3 from "bootstrap-vue-3";
+
+// Optional, since every component import their Bootstrap functionality
+// the following line is not necessary
+// import 'bootstrap' asdfasd
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAqsTB2ugBARb1Y44lElrqyWOG6kZmpYYQ",
-  authDomain: "baemin-vue-57bc1.firebaseapp.com",
-  projectId: "baemin-vue-57bc1",
-  storageBucket: "baemin-vue-57bc1.appspot.com",
-  messagingSenderId: "617109452175",
-  appId: "1:617109452175:web:beead616b0c0471ca387fa",
-  measurementId: "G-K29RZHM2M9",
+  apiKey: "AIzaSyATXZhOPR0GeDk10-J9Yl6K9YWAEI_OqxY",
+  authDomain: "baemin-vue.firebaseapp.com",
+  projectId: "baemin-vue",
+  storageBucket: "baemin-vue.appspot.com",
+  messagingSenderId: "805734936503",
+  appId: "1:805734936503:web:372284efc45c5cf97c8232",
+  measurementId: "G-L40L9QRPF6",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-createApp(App).use(router).mount("#app");
+
+createApp(App).use(router).use(BootstrapVue3).mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+/* eslint-disable prettier/prettier */
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
-  transpileDependencies: true,
+    transpileDependencies: true,
+    publicPath: process.env.NODE_DEV === "production" && process.env.DEPLOY != "firebase" ?
+        "/baemin_proj/" :
+        "/",
 });
